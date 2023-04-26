9d08a88527d8346cdd8d8258ce5c52976a28fc47
yousseffekry <mr-youssefsherif@hotmail.com>
2023-04-26 15:36:45 +0200

    Created the component for rendering the question

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import './App.css';
+import Question from "./components/Question";
 
 
 function App() {
     const [question, setQuestion] = useState();
-    const [answer, setAnswer] = useState();
+    const [correctAnswer, setCorrectAnswer] = useState();
+    const [incorrectAnswers, setIncorrectAnswers] = useState([]);
 
     useEffect(() => {
         fetch('https://opentdb.com/api.php?amount=1')
             .then(response => response.json())
             .then(json => {
                 setQuestion(json.results[0].question);
-                setAnswer(json.results[0].correct_answer);
+                setCorrectAnswer(json.results[0].correct_answer);
+                setIncorrectAnswers(json.results[0].incorrect_answers);
             })
             .catch(error => console.error(error));
     }, []);
 
     return (
-        <div>
-            <h1>{question}</h1>
-            <p>{answer}</p>
+        <div className="d-flex flex-wrap flex-column align-items-center mt-3 p-2">
+            <Question question={question} />
+            <p>{correctAnswer}</p>
+            <p>{incorrectAnswers.length > 1 && incorrectAnswers}</p>
         </div>
     );
 }
---
+++ src/components/Question.tsx
@@ -0,0 +1,7 @@
+const Question = (props: any) => {
+  return (
+      <h1 className="col-7 border-bottom pb-1">{props.question}</h1>
+  );
+}
+
+export default Question;
